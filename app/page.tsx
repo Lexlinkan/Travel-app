051621305c75a7cd7e05c9c46483d6829e01497b
Lexlinkan <alexander-lindqvist@live.se>
2023-11-06 23:10:40 +0100

    latest update 6/11

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1>
-      hello world
+    <h1 className='font-bold text-teal-500 text-2xl'> 
+      Hello world
     </h1>
   )
 }
